c15cd7cf77a21f701bfbf20e2cec995c1c5992a4
Matrix404x <ashishprasannan73@gmail.com>
2025-10-30 15:01:26 +0530

    used PUT for editing the resume

--- resume-builder-app/src/services/allAPI.js
+++ resume-builder-app/src/services/allAPI.js
@@ -1,7 +1,13 @@
 import commonAPI from "./common";
 import baseUrl from "./baseurl";
 
-// addResumeAPI
+// addResumeAPI - PUSH
 export const addResumeAPI = async(resume)=>{
     return await commonAPI("POST",`${baseUrl}/resumes`,resume)
+}
+
+// editResumeAPI - PUT
+
+export const editResumeAPI = async(id,resume)=>{
+    return await commonAPI("PUT",`${baseUrl}/resume/${id}`,resume)
 }
