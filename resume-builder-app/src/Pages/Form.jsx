9c7f16f28a8be6f958fa6bcaf65e77a9182e8ebc
Matrix404x <ashishprasannan73@gmail.com>
2025-10-30 13:58:00 +0530

    updated Forms

--- resume-builder-app/src/Pages/Form.jsx
+++ resume-builder-app/src/Pages/Form.jsx
@@ -5,67 +5,61 @@ import Preview from '../Components/Preview'
 
 function Form() {
 
-  const[userInput,setUserInput]=useState({
-    personalDetails:{
-      name:"",
-      jobTitle:"",
-      location:"",
-      email:"",
-      phone:"",
-      github:"",
-      linkedIn:"",
-      portfolio:""
+  const [userInput, setUserInput] = useState({
+    personalDetails: {
+      name: "",
+      jobTitle: "",
+      location: "",
+      email: "",
+      phone: "",
+      github: "",
+      linkedIn: "",
+      portfolio: ""
     },
-    education:{
-      course:"",
-      college:"",
-      university:"",
-      year:""
+    education: {
+      course: "",
+      college: "",
+      university: "",
+      year: ""
     },
-    experience:{
-      job:"",
-      company:"",
-      location:"",
-      duration:""
+    experience: {
+      job: "",
+      company: "",
+      location: "",
+      duration: ""
     },
-    skills:[],
-    summary:""
+    skills: [],
+    summary: ""
   })
 
-
+  const [finish, setFinish] = useState(false)
 
   return (
     <div>
-     <div className="row p-5">
-      {/* <div className="row">
-        <div className="col-3"></div>
-        <div className="col-8">
-          <Preview/>
+      <div className="row p-5">
+        {finish ?<div className="row">
+          <div className="col-3"></div>
+          <div className="col-8">
+            <Preview userInput={userInput} setUserInput={setUserInput} finish={finish} />
+          </div>
+          <div className="col-1"> </div>
+        </div>:
+
+        <div className='row p-5'>
+          <div className="col-6">
+
+            <Steps userInput={userInput} setUserInput={setUserInput} setFinish={setf} />
+
+          </div>
+
+          <div className="col-6">
+            <Preview userInput={userInput} />
+
+          </div>
 
         </div>
-        <div className="col-1"> </div>
-
-       
-
-
-
-      </div> */}
-
-      <div className='row p-5'>
-        <div className="col-6">
-
-          <Steps userInput={userInput} setUserInput={setUserInput}/>
-
-        </div>
-
-        <div className="col-6">
-          <Preview userInput={userInput}/>
-
-        </div>
-
+}
       </div>
-
-     </div>
     </div>
   )
 }
